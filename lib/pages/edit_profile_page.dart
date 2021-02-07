5239b60cf7ed6f22b561da782b8ea430bd7eca4e
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-07 14:29:16 +0900

    created app bar

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class EditProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('登録'),
+      ),
+    );
   }
 }
