8ee8102bd0bef8883fa439bcfbcd8323e976d753
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-08 07:51:06 +0900

    adjusted padding

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -7,14 +7,26 @@ class EditProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('登録'),
       ),
-      body: Center(
-        child: ClipOval(
-          child: Image.asset(
-            'assets/images/user.png',
-            width: 150,
-            height: 150,
-          ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          vertical: 46,
+          horizontal: 60,
         ),
+        children: [
+          Center(
+            child: ClipOval(
+              child: Image.asset(
+                'assets/images/user.png',
+                width: 150,
+                height: 150,
+              ),
+            ),
+          ),
+          const SizedBox(height: 46),
+          TextFormField(),
+          const SizedBox(height: 46),
+          TextFormField(),
+        ],
       ),
     );
   }
