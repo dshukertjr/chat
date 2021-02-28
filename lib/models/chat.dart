0e34f3f1a2d27d53b55011d8e28d73423ef1e57a
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-02-28 20:25:56 +0900

    created chat page

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -1,4 +1,5 @@
 import 'package:chat/models/firestore_user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 class Chat {
@@ -13,4 +14,11 @@ class Chat {
     @required this.text,
     @required this.createdAt,
   });
+
+  Chat.fromSnap(DocumentSnapshot snap)
+      : threadId = snap.data()['threadId'] as String,
+        user = FirestoreUser.fromMap(Map<String, dynamic>.from(
+            snap.data()['user'] as Map<dynamic, dynamic>)),
+        text = snap.data()['text'] as String,
+        createdAt = (snap.data()['createdAt'] as Timestamp)?.toDate();
 }
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,7 @@
+import 'package:chat/models/chat.dart';
+import 'package:chat/models/firestore_user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -15,44 +19,35 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 16,
-              ),
-              itemBuilder: (_, index) {
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    ClipOval(
-                      child: Image.asset(
-                        'assets/images/user.png',
-                        width: 30,
-                        height: 30,
-                      ),
+            child: StreamBuilder<List<Chat>>(
+                stream: FirebaseFirestore.instance
+                    .collection('chats')
+                    .where('threadId', isEqualTo: threadId)
+                    .snapshots()
+                    .map((snap) =>
+                        snap.docs.map((doc) => Chat.fromSnap(doc)).toList()),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(snapshot.error.toString()),
+                    );
+                  }
+                  final chats = snapshot.data;
+                  return ListView.builder(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
                     ),
-                    const SizedBox(width: 11),
-                    Flexible(
-                      child: Material(
-                        borderRadius: BorderRadius.circular(8),
-                        elevation: 4,
-                        color: Colors.white,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            '時間になったら連絡します。時間になったら連絡します。',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 11),
-                    Text('12:12'),
-                  ],
-                );
-              },
-              itemCount: 1,
-            ),
+                    itemBuilder: (_, index) {
+                      final chat = chats[index];
+                      return _ChatCell(chat);
+                    },
+                    itemCount: chats.length,
+                  );
+                }),
           ),
           Material(
             elevation: 10,
@@ -82,6 +77,48 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _ChatCell extends StatelessWidget {
+  const _ChatCell(
+    this.chat, {
+    Key key,
+  }) : super(key: key);
+
+  final Chat chat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ClipOval(
+          child: Image.asset(
+            'assets/images/user.png',
+            width: 30,
+            height: 30,
+          ),
+        ),
+        const SizedBox(width: 11),
+        Flexible(
+          child: Material(
+            borderRadius: BorderRadius.circular(8),
+            elevation: 4,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                chat.text,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 11),
+        Text('12:12'),
+      ],
     );
   }
 }
